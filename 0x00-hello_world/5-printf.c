2fd3dcfb4fbe10387adb37c926ab0e6f9209fb08
vic-1998 <victor.mplaza98@gmail.com>
2020-09-16 20:08:46 +0000

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ * 5-printf.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints with proper grammar, but the outcome is a piece of art,
+ * Description: Learning uses printf
+ * Return: void
+ */
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
