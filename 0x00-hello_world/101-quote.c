297217a13207959c62dc27ddd70767a7d881ccbc
vic-1998 <victor.mplaza98@gmail.com>
2020-09-16 21:30:30 +0000

    Quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/*
+ * 101-quote.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Write "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Description: Learning write
+ * Return: returns 1
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 44);
+return (1);
+}
