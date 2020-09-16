46b724cce92075d674cb638bbde3de62f7563d0f
vic-1998 <victor.mplaza98@gmail.com>
2020-09-16 18:53:58 +0000

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - puts line t
+ * Description: uses puts to output message
+ * Return: void
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
