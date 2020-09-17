d06609dd963c13aa954abc8ca60d71f8c56c9bac
vic-1998 <victor.mplaza98@gmail.com>
2020-09-17 21:31:30 +0000

    Print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+/*
+ * 7-print_tebahpla.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Print tebahpla
+ * Description: Conditionals in C
+ * Return: void
+ */
+int main(void)
+{
+	char betty;
+
+	for (betty = 'z'; betty >= 'a'; betty--)
+		putchar(betty);
+
+	putchar("\n");
+
+	return (0);
+}
