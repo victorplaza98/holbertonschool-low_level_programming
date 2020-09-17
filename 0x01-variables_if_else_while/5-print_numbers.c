31ab91e63d9d2296c999f9ac56116121672e30fa
vic-1998 <victor.mplaza98@gmail.com>
2020-09-17 20:40:07 +0000

    Print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * 5-print_numbers.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Print_number
+ * Description: Conditionals in C
+ * Return: void
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+
+	return (0);
+}
