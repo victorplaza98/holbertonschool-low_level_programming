d898cae4a59904eb20de35ec5dfe1fe357efcfc2
vic-1998 <victor.mplaza98@gmail.com>
2020-09-17 23:19:07 +0000

    Print_comb2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+/*
+ * 10-print_comb2.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Print_comb2
+ * Description: Conditionals in C
+ * Return: void
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 =0; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if(num1 == 9 && num2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
