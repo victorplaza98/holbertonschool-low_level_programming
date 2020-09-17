465db036b42ecb746f4d6240cdfa985bc8c64d99
vic-1998 <victor.mplaza98@gmail.com>
2020-09-17 23:37:43 +0000

    Print_comb2

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -15,17 +15,20 @@ int main(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 =0; num2 < 10; num2++)
+		for (num2 = 0; num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if(num1 == 9 && num2 == 9)
+			if (num1 == 9 && num2 == 9)
 				continue;
+			
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
