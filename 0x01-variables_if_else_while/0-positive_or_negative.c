a0b1080f1a64252da2c6753f67d1db8ad3556a37
vic-1998 <victor.mplaza98@gmail.com>
2020-09-17 16:16:30 +0000

    Positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+/*
+ * 0-positive_or_negative.c
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Positive or negative
+ * Description: Conditionals in C
+ * Return: void
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
