7fbddc3d3c8128f16b126a5722865f4942264505
vic-1998 <victor.mplaza98@gmail.com>
2020-09-17 22:04:36 +0000

    Print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+/*
+ * 9-print_comb.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Print_numberz
+ * Description: Conditional in C
+ * Return: void
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
