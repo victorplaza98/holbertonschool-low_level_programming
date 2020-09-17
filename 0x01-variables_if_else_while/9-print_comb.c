1fa6a2a7ab1c081554df16b2798d405714ae9aea
vic-1998 <victor.mplaza98@gmail.com>
2020-09-17 23:30:04 +0000

    Print-comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,10 +16,11 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar('\n');
 
 	return (0);
