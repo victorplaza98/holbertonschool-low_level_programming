b810ae228621b699a2d3f5a124e84fd772f67c1f
vic-1998 <victor.mplaza98@gmail.com>
2020-09-18 04:04:29 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,24 +13,21 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 100; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = 0; num2 < 100; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			if (num1 < num2)
-			{
-				putchar((num1 / 10) + '0');
-				putchar((num1 % 10) + '0');
-				putchar(' ');
-				putchar((num2 / 10) + '0');
-				putchar((num2 % 10) + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-				if (num1 < 99)
-				{
-				putchar(',');
-				putchar(' ');
-				}
-			}
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
