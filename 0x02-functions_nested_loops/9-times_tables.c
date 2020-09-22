e4abd36b67a0d03a4109cc13bd4325899087be89
vic-1998 <victor.mplaza98@gmail.com>
2020-09-22 15:10:19 +0000

    Times_table

--- 0x02-functions_nested_loops/9-times_tables.c
+++ 0x02-functions_nested_loops/9-times_tables.c
@@ -6,4 +6,26 @@
  */
 void times_table(void)
 {
+	int a, b, x, y, z;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 0; a < 10; a++)
+		{
+			x = a * b;
+			y = x / 10;
+			z = x % 10;
+			if (a > 0)
+				_putchar(' ');
+			if ((x < 10) && (a > 0))
+				_putchar(' ');
+			else if (x > 9)
+				_putchar(y + '0');
+			_putchar(z + '0');
+			if (a < 9)
+				_putchar(',');
+			else if (a == 9)
+				_putchar('\n');
+		}
+	}
 }
