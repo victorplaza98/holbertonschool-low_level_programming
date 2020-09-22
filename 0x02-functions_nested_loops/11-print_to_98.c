a2d2ec70acdf7cb1a1fa8c865c95cb05e2ab8605
vic-1998 <victor.mplaza98@gmail.com>
2020-09-22 15:42:04 +0000

    Changes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,4 +4,17 @@
  * @n: number start
  * Return: 0
  */
-
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n < 98; n++)
+			printf("%i, ", n);
+	}
+	else if (n > 98)
+	{
+		for (; n > 98; n--)
+			printf("%i, ", n);
+	}
+	printf("98\n");
+}
