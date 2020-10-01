9b4f3ba482cc1914b2ab9630fb82525b2ed2196b
vic-1998 <victor.mplaza98@gmail.com>
2020-10-01 18:48:35 +0000

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strncpy - string with n bytes
+ * @dest: copy the src
+ * @src: String 
+ * @n: number of bytes
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0' && i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+
+	return (dest);
+}
