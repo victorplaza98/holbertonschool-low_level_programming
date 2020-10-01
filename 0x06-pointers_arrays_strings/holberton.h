7d85154d126071306c87c705d100c7a93d33f9ba
vic-1998 <victor.mplaza98@gmail.com>
2020-10-01 18:22:11 +0000

    holberton

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,4 +1,14 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
 #ifndef _STRCAT_H
 #define _STRCAT_H
 char *_strcat(char *dest, char *src);
+#endif
+
+#ifndef _STRNCAT_H
+#define _STRNCAT_H
+char *_strncat(char *dest, char *src, int n);
 #endif
