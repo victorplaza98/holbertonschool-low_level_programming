0b4bb61e4f0c58589f0003702e13bafef77e779d
vic-1998 <victor.mplaza98@gmail.com>
2020-10-05 16:56:59 +0000

    4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strpbrk - funtion
+ * @s: frist string
+ * @accept: second string
+ * Return: insigned int
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int x, y;
+
+	for (x = 0; s[x]; x++)
+	{
+		for (y = 0; accept[y]; y++)
+		{
+			if (s[x] == accept[y])
+				break;
+		}
+		if (accept[y])
+			return (s + x);
+	}
+	return (0);
+}
