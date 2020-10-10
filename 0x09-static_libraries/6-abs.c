69be030c6d2545f3605fe812575b1bd860c5a019
vic-1998 <victor.mplaza98@gmail.com>
2020-10-10 21:51:44 +0000

    6-abs

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _abs - Returns value of n
+ * @n: integer value
+ * Description: The program value of a number
+ * Return: n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
