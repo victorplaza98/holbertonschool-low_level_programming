f6b550107c702e981d27b2c78afc5afcd0052b78
vic-1998 <victor.mplaza98@gmail.com>
2020-10-10 21:52:12 +0000

    9-strcpy

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcpy - given string
+ * @dest: array
+ * @src: size of array
+ * Return: 1
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	if (*(src + i) == '\0')
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
