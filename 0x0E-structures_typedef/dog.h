458e4d1bc052651411a47d3d8b4c081d3af8b39e
vic-1998 <victor.mplaza98@gmail.com>
2020-10-19 19:18:59 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,5 +12,9 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+void free_dog(dog_t *d);
 
 #endif
