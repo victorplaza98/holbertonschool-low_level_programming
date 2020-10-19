bea747fa8f5561077b7e82ded87a2716141b984d
vic-1998 <victor.mplaza98@gmail.com>
2020-10-19 19:13:19 +0000

    1-init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,7 +5,6 @@
  * @name: char to set name of the struc dog
  * @age: int to set value of the struct dog
  * @owner: char to set name of the struct dog
- *
  * Return: Void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
