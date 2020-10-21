4a2ed287ac07cfea0305b513e6f46b53e43b8d1c
vic-1998 <victor.mplaza98@gmail.com>
2020-10-21 23:02:09 +0000

    function only

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,11 +1,22 @@
 #include "function_pointers.h"
-
 /**
- * op_add -
- *
- * Return: 
+ * main - Entry point
+ * @argc: lenght of arguments
+ * @argv: array of arguments
+ * Return: (0) Exit
  */
-int op_add(int a, int b)
+int main(int argc, char *argv[])
 {
+	int result;
 
+	if (argc == 4)
+	{
+		result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+		printf("%d\n", result);
+	} else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (0);
 }
