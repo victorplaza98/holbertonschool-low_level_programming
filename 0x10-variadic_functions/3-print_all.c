6b6dcbe936fe40fb91bb8ee965359f9911538b9a
vic-1998 <victor.mplaza98@gmail.com>
2020-10-23 20:40:17 +0000

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,11 +1,48 @@
 #include "variadic_functions.h"
-
 /**
- * print_all -
- *
- * Return: 
+ * print_all - Function that returns pointer to funcion
+ * @format: list of argument types passed to the function
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
+	va_list list;
+	unsigned int x = 0, y = 1;
+	char *h;
 
+	while (format)
+	{
+		va_start(list, format);
+		while (format[x])
+		{
+			y = 1;
+			switch (format[x])
+			{
+			case 'c':
+				printf("%c", va_arg(list, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(list, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(list, double));
+				break;
+			case 's':
+				h = va_arg(list, char *);
+				if (h == NULL)
+					h = "(nil)";
+				printf("%s", h);
+				break;
+			default:
+				y = 0;
+				break;
+			}
+			if (format[x + 1] && y)
+				printf(", ");
+			x++;
+		}
+		va_end(list);
+		break;
+	}
+	printf("\n");
 }
