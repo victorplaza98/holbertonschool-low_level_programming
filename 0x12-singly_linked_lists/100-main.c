828172b705a00ffed0a11d50eb1f08665caef723
vic-1998 <victor.mplaza@gmail.com>
2020-10-29 14:40:37 +0000

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
+}
+julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra 100-main.c 100-first.c -o first
+julien@ubuntu:~/$ ./first 
+You're beat! and yet, you must allow,
+I bore my house upon my back!
+(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)
+julien@ubuntu:~/$ 
