7d069c8fd03084eb0b11bd6a8badc397c3b39407
vic-1998 <victor.mplaza98@gmail.com>
2020-11-04 14:55:17 +0000

    Add 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * sum_listint -
- *
- * Return: 
+ * sum_listint - Function that returns the sum
+ * @head: Pointed to head of a list
+ * Return: sum
  */
 int sum_listint(listint_t *head)
 {
+	int sum = 0;
 
+	if (head == NULL)
+		return (sum);
+
+	sum = head->n + sum_listint(head->next);
+
+	return (sum);
 }
