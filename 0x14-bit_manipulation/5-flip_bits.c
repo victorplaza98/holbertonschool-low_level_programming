9f836b681c9661b2e77103248fba72c87d9cec83
vic-1998 <victor.mplaza98@gmail.com>
2020-11-05 23:21:34 +0000

    Add  5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,23 @@
 #include "holberton.h"
 
 /**
- * flip_bits -
- *
- * Return: 
+ * flip_bits -  function that returns the number of bits you would need
+ * @n: first number
+ * @m: second number
+ * Return: number of bits
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int count;
+	int num = 0;
 
+	count = n ^ m;
+
+	while (count)
+	{
+		num += count & 1;
+		count = count >> 1;
+	}
+
+	return (num);
 }
