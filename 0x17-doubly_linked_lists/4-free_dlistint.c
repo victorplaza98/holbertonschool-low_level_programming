90e1bfd9e04f7efe9b2d2c097f4ebf9c09e7f265
vic-1998 <victor.mplaza98@gmail.com>
2020-12-13 16:13:46 +0000

    Write a function that frees a dlistint_t list.

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * free_dlistint -
- *
- * Return: 
+ * free_dlistint - Function that count all the elements
+ * of a dlistint_t list
+ * @head: double Pointer with the addres of head of  list
+ * Return: Void
  */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *temp;
 
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
