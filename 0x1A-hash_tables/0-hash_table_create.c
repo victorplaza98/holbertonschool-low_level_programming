c4f9442a9fcabe43e1e4ffe3a67eb92ee4cf901a
vic-1998 <victor.mplaza98@gmail.com>
2021-01-15 16:02:39 +0000

    Fix error.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
+	hash_table_t *hash_table = NULL;
 	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
