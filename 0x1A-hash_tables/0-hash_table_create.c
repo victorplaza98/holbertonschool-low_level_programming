31570361858d2d0c7f5810fdff4a22951133cb1a
vic-1998 <victor.mplaza98@gmail.com>
2021-01-15 15:59:51 +0000

    Fix error.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,20 +6,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash;
+	hash_table_t *hash_table;
 	unsigned long int i;
 
-	hash = malloc(sizeof(hash_table_t));
-	if (hash == NULL)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
-	hash->size = size;
-	hash->array = malloc(size * sizeof(hash_table_t *));
-	if (hash->array == NULL)
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(hash);
+		free(hash_table);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
-		hash->array[i] = NULL;
-	return (hash);
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
