49c91c1c8a7a214a8bfc741d9b2f95eeb3a5924d
vic-1998 <victor.mplaza98@gmail.com>
2021-01-15 16:11:11 +0000

    Readme

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,11 +13,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 		return (NULL);
+
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!hash_table->array)
 		return (NULL);
+
 	for (i = 0; i < size; i++)
 		hash_table->array[i] = NULL;
+
 	return (hash_table);
 }
