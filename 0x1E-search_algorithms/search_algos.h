0b9a3eb8e31fabce6ae6c1d3e372bbf7ee067de4
vic-1998 <victor.mplaza98@gmail.com>
2021-04-20 16:39:24 -0500

    Add search.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,23 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-	int n;
-	size_t index;
-	struct listint_s *next;
-} listint_t;
-
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
