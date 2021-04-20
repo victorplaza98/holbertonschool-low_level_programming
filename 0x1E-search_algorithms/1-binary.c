a956c53b22a5b00d7c1b7eed3f19b608dc3bd374
vic-1998 <victor.mplaza98@gmail.com>
2021-04-20 16:53:09 -0500

    Binary search.

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,24 +1,5 @@
 #include "search_algos.h"
-/**
- * print_array - prints a array
- * @array: array to print
- * @size: size of array
- *
- * return: void
- */
-void print_array(int *array, size_t size)
-{
-	size_t i;
 
-	printf("Searching in array:");
-	for (i = 0; i < size; i++)
-	{
-		printf(" %d", array[i]);
-		if (i != size - 1)
-			printf(",");
-	}
-	printf("\n");
-}
 /**
  * binary_search - searches for a value in a sorted array
  * @array:  pointer to the first element
@@ -28,24 +9,25 @@ void print_array(int *array, size_t size)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t l, m, r;
+	size_t i, left = 0, right = size - 1, temp = 0;
 
-	if (array != NULL && size > 0)
-	{
-		l = 0;
-		r = size - 1;
-		print_array(array + l, r + 1 - l);
-		while (l < r)
+	if (array)
+		while (left <= right)
 		{
-			m = (l + r) / 2;
-			if (array[m] < value)
-				l = m + 1;
-			else if (array[m] > value)
-				r = m;
+			temp = (left + right) / 2;
+
+			/* Prints the current array */
+			printf("Searching in array: ");
+			for (i = left; i <= right; i++)
+				i != right ? printf("%d, ", array[i]) : printf("%d", array[i]);
+			printf("\n");
+
+			if (array[temp] < value)
+				left = temp + 1;
+			else if (array[temp] > value)
+				right = temp - 1;
 			else
-				return (m);
-			print_array(array + l, r + 1 - l);
+				return (temp);
 		}
-	}
 	return (-1);
 }
